b16c56ddadf160e853c30ee49d5e98f2995ae7d3
Palak Avlani <palak.avlani@forge-d.com>
2026-01-19 12:52:01 +0530

    Configuration for Swagger UI added

--- src/main/java/io/ledger/wallet/config/SecurityConfig.java
+++ src/main/java/io/ledger/wallet/config/SecurityConfig.java
@@ -47,7 +47,9 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/auth/**").permitAll() 
+                .requestMatchers("/api/v1/auth/**", "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html").permitAll() 
                 .anyRequest().authenticated()                   
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
